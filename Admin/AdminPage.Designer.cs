c4a46cfb4caf703ab83ce870ed647e5ff0d28771
DysonDop <dylan.mansul@gmail.com>
2024-10-01 08:43:26 +0800

    amdin
    sjsis

---
+++ Admin/AdminPage.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Assignment
+{
+    partial class AdminPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminPage));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Btn_editServices = new System.Windows.Forms.Button();
+            this.Btn_deleteServices = new System.Windows.Forms.Button();
+            this.Btn_AddService = new System.Windows.Forms.Button();
+            this.Btn_StaffA = new System.Windows.Forms.Button();
+            this.btn_Adminlogout = new System.Windows.Forms.Button();
+            this.btn_ServiceReport = new System.Windows.Forms.Button();
+            this.btn_CustomerFeed = new System.Windows.Forms.Button();
+            this.btn_ServiceInfo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_addstaf = new System.Windows.Forms.Button();
+            this.Btn_deletestaff = new System.Windows.Forms.Button();
+            this.AdminPContainer = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel1.Controls.Add(this.Btn_editServices);
+            this.panel1.Controls.Add(this.Btn_deleteServices);
+            this.panel1.Controls.Add(this.Btn_AddService);
+            this.panel1.Controls.Add(this.Btn_StaffA);
+            this.panel1.Controls.Add(this.btn_Adminlogout);
+            this.panel1.Controls.Add(this.btn_ServiceReport);
+            this.panel1.Controls.Add(this.btn_CustomerFeed);
+            this.panel1.Controls.Add(this.btn_ServiceInfo);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.btn_addstaf);
+            this.panel1.Controls.Add(this.Btn_deletestaff);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(252, 1036);
+            this.panel1.TabIndex = 0;
+            // 
+            // Btn_editServices
+            // 
+            this.Btn_editServices.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Btn_editServices.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_editServices.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Btn_editServices.Location = new System.Drawing.Point(6, 644);
+            this.Btn_editServices.Margin = new System.Windows.Forms.Padding(6);
+            this.Btn_editServices.Name = "Btn_editServices";
+            this.Btn_editServices.Size = new System.Drawing.Size(240, 53);
+            this.Btn_editServices.TabIndex = 12;
+            this.Btn_editServices.Text = "Edit";
+            this.Btn_editServices.UseVisualStyleBackColor = true;
+            this.Btn_editServices.Visible = false;
+            // 
+            // Btn_deleteServices
+            // 
+            this.Btn_deleteServices.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Btn_deleteServices.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_deleteServices.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Btn_deleteServices.Location = new System.Drawing.Point(8, 699);
+            this.Btn_deleteServices.Margin = new System.Windows.Forms.Padding(6);
+            this.Btn_deleteServices.Name = "Btn_deleteServices";
+            this.Btn_deleteServices.Size = new System.Drawing.Size(240, 53);
+            this.Btn_deleteServices.TabIndex = 11;
+            this.Btn_deleteServices.Text = "Delete";
+            this.Btn_deleteServices.UseVisualStyleBackColor = true;
+            this.Btn_deleteServices.Visible = false;
+            // 
+            // Btn_AddService
+            // 
+            this.Btn_AddService.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Btn_AddService.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_AddService.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Btn_AddService.Location = new System.Drawing.Point(8, 599);
+            this.Btn_AddService.Margin = new System.Windows.Forms.Padding(6);
+            this.Btn_AddService.Name = "Btn_AddService";
+            this.Btn_AddService.Size = new System.Drawing.Size(238, 53);
+            this.Btn_AddService.TabIndex = 10;
+            this.Btn_AddService.Text = "Add";
+            this.Btn_AddService.UseVisualStyleBackColor = true;
+            this.Btn_AddService.Visible = false;
+            // 
+            // Btn_StaffA
+            // 
+            this.Btn_StaffA.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Btn_StaffA.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_StaffA.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Btn_StaffA.Location = new System.Drawing.Point(2, 291);
+            this.Btn_StaffA.Margin = new System.Windows.Forms.Padding(6);
+            this.Btn_StaffA.Name = "Btn_StaffA";
+            this.Btn_StaffA.Size = new System.Drawing.Size(238, 85);
+            this.Btn_StaffA.TabIndex = 7;
+            this.Btn_StaffA.Text = "Manage Staff";
+            this.Btn_StaffA.UseVisualStyleBackColor = true;
+            this.Btn_StaffA.Click += new System.EventHandler(this.Btn_StaffA_Click);
+            this.Btn_StaffA.MouseEnter += new System.EventHandler(this.BtnManageStaff_MouseEnter);
+            this.Btn_StaffA.MouseLeave += new System.EventHandler(this.BtnManageStaff_MouseLeave);
+            // 
+            // btn_Adminlogout
+            // 
+            this.btn_Adminlogout.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn_Adminlogout.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Adminlogout.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_Adminlogout.Location = new System.Drawing.Point(14, 968);
+            this.btn_Adminlogout.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_Adminlogout.Name = "btn_Adminlogout";
+            this.btn_Adminlogout.Size = new System.Drawing.Size(232, 68);
+            this.btn_Adminlogout.TabIndex = 6;
+            this.btn_Adminlogout.Text = "Log Out";
+            this.btn_Adminlogout.UseVisualStyleBackColor = true;
+            this.btn_Adminlogout.Click += new System.EventHandler(this.btn_Adminlogout_Click);
+            // 
+            // btn_ServiceReport
+            // 
+            this.btn_ServiceReport.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn_ServiceReport.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ServiceReport.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_ServiceReport.Location = new System.Drawing.Point(8, 880);
+            this.btn_ServiceReport.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_ServiceReport.Name = "btn_ServiceReport";
+            this.btn_ServiceReport.Size = new System.Drawing.Size(238, 85);
+            this.btn_ServiceReport.TabIndex = 5;
+            this.btn_ServiceReport.Text = "Service Reports";
+            this.btn_ServiceReport.UseVisualStyleBackColor = true;
+            this.btn_ServiceReport.Click += new System.EventHandler(this.btn_ServiceReport_Click);
+            // 
+            // btn_CustomerFeed
+            // 
+            this.btn_CustomerFeed.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn_CustomerFeed.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_CustomerFeed.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_CustomerFeed.Location = new System.Drawing.Point(6, 764);
+            this.btn_CustomerFeed.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_CustomerFeed.Name = "btn_CustomerFeed";
+            this.btn_CustomerFeed.Size = new System.Drawing.Size(246, 104);
+            this.btn_CustomerFeed.TabIndex = 4;
+            this.btn_CustomerFeed.Text = "Customer Feedback";
+            this.btn_CustomerFeed.UseVisualStyleBackColor = true;
+            this.btn_CustomerFeed.Click += new System.EventHandler(this.btn_CustomerFeed_Click);
+            // 
+            // btn_ServiceInfo
+            // 
+            this.btn_ServiceInfo.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn_ServiceInfo.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ServiceInfo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_ServiceInfo.Location = new System.Drawing.Point(6, 499);
+            this.btn_ServiceInfo.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_ServiceInfo.Name = "btn_ServiceInfo";
+            this.btn_ServiceInfo.Size = new System.Drawing.Size(246, 100);
+            this.btn_ServiceInfo.TabIndex = 3;
+            this.btn_ServiceInfo.Text = "Manage Service Info";
+            this.btn_ServiceInfo.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 240);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Admin";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.pictureBox1.Image = global::Assignment.Properties.Resources._8UtZqX_LogoMakr;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(252, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_addstaf
+            // 
+            this.btn_addstaf.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn_addstaf.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_addstaf.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_addstaf.Location = new System.Drawing.Point(2, 378);
+            this.btn_addstaf.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_addstaf.Name = "btn_addstaf";
+            this.btn_addstaf.Size = new System.Drawing.Size(238, 53);
+            this.btn_addstaf.TabIndex = 8;
+            this.btn_addstaf.Text = "Add";
+            this.btn_addstaf.UseVisualStyleBackColor = true;
+            this.btn_addstaf.Visible = false;
+            this.btn_addstaf.Click += new System.EventHandler(this.Btn_addstaf_Click);
+            // 
+            // Btn_deletestaff
+            // 
+            this.Btn_deletestaff.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Btn_deletestaff.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_deletestaff.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Btn_deletestaff.Location = new System.Drawing.Point(0, 434);
+            this.Btn_deletestaff.Margin = new System.Windows.Forms.Padding(6);
+            this.Btn_deletestaff.Name = "Btn_deletestaff";
+            this.Btn_deletestaff.Size = new System.Drawing.Size(240, 53);
+            this.Btn_deletestaff.TabIndex = 9;
+            this.Btn_deletestaff.Text = "Delete";
+            this.Btn_deletestaff.UseVisualStyleBackColor = true;
+            this.Btn_deletestaff.Visible = false;
+            this.Btn_deletestaff.Click += new System.EventHandler(this.Btn_deletestaff_Click);
+            // 
+            // AdminPContainer
+            // 
+            this.AdminPContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AdminPContainer.Location = new System.Drawing.Point(252, 0);
+            this.AdminPContainer.Margin = new System.Windows.Forms.Padding(6);
+            this.AdminPContainer.Name = "AdminPContainer";
+            this.AdminPContainer.Size = new System.Drawing.Size(1500, 1036);
+            this.AdminPContainer.TabIndex = 1;
+            // 
+            // AdminPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1752, 1036);
+            this.Controls.Add(this.AdminPContainer);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "AdminPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AdminPage";
+            this.Load += new System.EventHandler(this.AdminPageLoad);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel AdminPContainer;
+        private System.Windows.Forms.Button btn_Adminlogout;
+        private System.Windows.Forms.Button btn_ServiceReport;
+        private System.Windows.Forms.Button btn_CustomerFeed;
+        private System.Windows.Forms.Button btn_ServiceInfo;
+        private System.Windows.Forms.Button Btn_StaffA;
+        private System.Windows.Forms.Button Btn_deletestaff;
+        private System.Windows.Forms.Button btn_addstaf;
+        private System.Windows.Forms.Button Btn_deleteServices;
+        private System.Windows.Forms.Button Btn_AddService;
+        private System.Windows.Forms.Button Btn_editServices;
+    }
+}
---
+++ Admin/AdminPage.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment
+{
+    public partial class AdminPage : Form
+    {
+        public AdminPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AdminPageLoad(object sender, EventArgs e)
+        {
+            btn_addstaf.Visible = false;
+            Btn_deletestaff.Visible = false;
+
+            btn_ServiceInfo.Top = Btn_StaffA.Bottom + 10;
+            btn_CustomerFeed.Top = btn_ServiceInfo.Bottom + 10;
+            btn_ServiceReport.Top = btn_CustomerFeed.Bottom + 10;
+            btn_Adminlogout.Top = btn_ServiceReport.Bottom + 10;
+        }
+
+
+        // Add the MouseEnter event to the "Manage Staff" button
+        private void BtnManageStaff_MouseEnter(object sender, EventArgs e)
+        {
+            btn_addstaf.Visible = true;
+            Btn_deletestaff.Visible = true;
+
+            btn_addstaf.Top = Btn_StaffA.Bottom + 1;
+            Btn_deletestaff.Top = btn_addstaf.Bottom + 1;
+
+            btn_ServiceInfo.Top = Btn_deletestaff.Bottom + 3;
+            btn_CustomerFeed.Top = btn_ServiceInfo.Bottom + 3;
+            btn_ServiceReport.Top = btn_CustomerFeed.Bottom + 3;
+            btn_Adminlogout.Top = btn_ServiceReport.Bottom + 2;
+
+        }
+
+        // Add the MouseLeave event to hide the buttons when not hovering
+        private void BtnManageStaff_MouseLeave(object sender, EventArgs e)
+        {
+            if (!btn_addstaf.ClientRectangle.Contains(btn_addstaf.PointToClient(Cursor.Position)) &&
+                !Btn_deletestaff.ClientRectangle.Contains(Btn_deletestaff.PointToClient(Cursor.Position)))
+            {
+                btn_addstaf.Visible = false;
+                Btn_deletestaff.Visible = false;
+
+                btn_ServiceInfo.Top = Btn_StaffA.Bottom + 10;
+                btn_CustomerFeed.Top = btn_ServiceInfo.Bottom + 10;
+                btn_ServiceReport.Top = btn_CustomerFeed.Bottom + 10;
+                btn_Adminlogout.Top = btn_ServiceReport.Bottom + 10;
+            }
+        }
+
+
+
+        private void AddUserControl(UserControl userControl)
+        {
+            userControl.Dock = DockStyle.Fill;
+            AdminPContainer.Controls.Clear();
+            AdminPContainer.Controls.Add(userControl);
+            userControl.BringToFront();
+        }
+
+        private void Btn_StaffA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_addstaf_Click(object sender, EventArgs e)
+        {
+            Add_Staff uc = new Add_Staff();
+            AddUserControl(uc);
+        }
+
+        private void Btn_deletestaff_Click(object sender, EventArgs e)
+        {
+            DeleteStaff uc = new DeleteStaff();
+            AddUserControl(uc); 
+        }
+
+        private void btn_CustomerFeed_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_ServiceReport_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Adminlogout_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
