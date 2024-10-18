bf5389847196ea5ab3f52bdfb4aeba3f11f7ff34
Josh5620 <josh.ong2005@gmail.com>
2024-10-18 15:32:37 +0800

    werwewer
    werwerwerwerwerwer

--- WindowsFormsApp2/RecepPage.Designer.cs
+++ WindowsFormsApp2/RecepPage.Designer.cs
@@ -28,9 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.ExitButton = new System.Windows.Forms.Button();
+            this.ControlPanel = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.ProfBtn = new System.Windows.Forms.Button();
             this.InvButton = new System.Windows.Forms.Button();
             this.CheckBtn = new System.Windows.Forms.Button();
@@ -38,13 +45,10 @@
             this.DelCust = new System.Windows.Forms.Button();
             this.AddCust = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.ControlPanel = new System.Windows.Forms.Panel();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.ControlPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -61,7 +65,6 @@
             // panel2
             // 
             this.panel2.AutoScroll = true;
-            this.panel2.Controls.Add(this.ExitButton);
             this.panel2.Controls.Add(this.ProfBtn);
             this.panel2.Controls.Add(this.InvButton);
             this.panel2.Controls.Add(this.CheckBtn);
@@ -69,6 +72,8 @@
             this.panel2.Controls.Add(this.DelCust);
             this.panel2.Controls.Add(this.AddCust);
             this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.ExitButton);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
@@ -86,96 +91,14 @@
             this.ExitButton.UseVisualStyleBackColor = true;
             this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
             // 
-            // ProfBtn
-            // 
-            this.ProfBtn.Dock = System.Windows.Forms.DockStyle.Top;
-            this.ProfBtn.Location = new System.Drawing.Point(0, 222);
-            this.ProfBtn.Name = "ProfBtn";
-            this.ProfBtn.Size = new System.Drawing.Size(151, 41);
-            this.ProfBtn.TabIndex = 8;
-            this.ProfBtn.Text = "Profile";
-            this.ProfBtn.UseVisualStyleBackColor = true;
-            this.ProfBtn.Click += new System.EventHandler(this.ProfBtn_Click);
-            // 
-            // InvButton
-            // 
-            this.InvButton.Dock = System.Windows.Forms.DockStyle.Top;
-            this.InvButton.Location = new System.Drawing.Point(0, 181);
-            this.InvButton.Name = "InvButton";
-            this.InvButton.Size = new System.Drawing.Size(151, 41);
-            this.InvButton.TabIndex = 7;
-            this.InvButton.Text = "View Inventory";
-            this.InvButton.UseVisualStyleBackColor = true;
-            this.InvButton.Click += new System.EventHandler(this.InvButton_Click);
-            // 
-            // CheckBtn
-            // 
-            this.CheckBtn.Dock = System.Windows.Forms.DockStyle.Top;
-            this.CheckBtn.Location = new System.Drawing.Point(0, 140);
-            this.CheckBtn.Name = "CheckBtn";
-            this.CheckBtn.Size = new System.Drawing.Size(151, 41);
-            this.CheckBtn.TabIndex = 5;
-            this.CheckBtn.Text = "Check In / Out";
-            this.CheckBtn.UseVisualStyleBackColor = true;
-            this.CheckBtn.Click += new System.EventHandler(this.CheckBtn_Click);
-            // 
-            // ServBtn
-            // 
-            this.ServBtn.Dock = System.Windows.Forms.DockStyle.Top;
-            this.ServBtn.Location = new System.Drawing.Point(0, 99);
-            this.ServBtn.Name = "ServBtn";
-            this.ServBtn.Size = new System.Drawing.Size(151, 41);
-            this.ServBtn.TabIndex = 4;
-            this.ServBtn.Text = "Service Appointment";
-            this.ServBtn.UseVisualStyleBackColor = true;
-            this.ServBtn.Click += new System.EventHandler(this.ServBtn_Click);
-            // 
-            // DelCust
-            // 
-            this.DelCust.Dock = System.Windows.Forms.DockStyle.Top;
-            this.DelCust.Location = new System.Drawing.Point(0, 70);
-            this.DelCust.Name = "DelCust";
-            this.DelCust.Size = new System.Drawing.Size(151, 29);
-            this.DelCust.TabIndex = 3;
-            this.DelCust.Text = "Delete";
-            this.DelCust.UseVisualStyleBackColor = true;
-            this.DelCust.Click += new System.EventHandler(this.DelCust_Click);
-            this.DelCust.MouseLeave += new System.EventHandler(this.DelBtnLeave);
-            this.DelCust.MouseHover += new System.EventHandler(this.DelCust_Hover);
-            // 
-            // AddCust
-            // 
-            this.AddCust.Dock = System.Windows.Forms.DockStyle.Top;
-            this.AddCust.Location = new System.Drawing.Point(0, 41);
-            this.AddCust.Name = "AddCust";
-            this.AddCust.Size = new System.Drawing.Size(151, 29);
-            this.AddCust.TabIndex = 2;
-            this.AddCust.Text = "Add";
-            this.AddCust.UseVisualStyleBackColor = true;
-            this.AddCust.Click += new System.EventHandler(this.AddCust_Click);
-            this.AddCust.MouseLeave += new System.EventHandler(this.AddBtnLeave);
-            this.AddCust.MouseHover += new System.EventHandler(this.AddCust_Hover);
-            // 
-            // button1
-            // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(151, 41);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Customer Management";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
-            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
-            // 
             // ControlPanel
             // 
             this.ControlPanel.Controls.Add(this.listBox1);
             this.ControlPanel.Controls.Add(this.button2);
             this.ControlPanel.Controls.Add(this.label1);
-            this.ControlPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ControlPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.ControlPanel.Location = new System.Drawing.Point(151, 0);
-            this.ControlPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ControlPanel.Margin = new System.Windows.Forms.Padding(2);
             this.ControlPanel.Name = "ControlPanel";
             this.ControlPanel.Size = new System.Drawing.Size(642, 511);
             this.ControlPanel.TabIndex = 2;
@@ -205,9 +128,108 @@
             this.label1.Location = new System.Drawing.Point(135, 218);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(353, 37);
+            this.label1.Size = new System.Drawing.Size(248, 37);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Brand Logo Image here";
+            this.label1.Text = "PROFILE HERE";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::WindowsFormsApp2.Properties.Resources.CarCare_Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(151, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ProfBtn
+            // 
+            this.ProfBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ProfBtn.Location = new System.Drawing.Point(0, 370);
+            this.ProfBtn.Name = "ProfBtn";
+            this.ProfBtn.Size = new System.Drawing.Size(151, 41);
+            this.ProfBtn.TabIndex = 17;
+            this.ProfBtn.Text = "Profile";
+            this.ProfBtn.UseVisualStyleBackColor = true;
+            this.ProfBtn.Click += new System.EventHandler(this.ProfBtn_Click);
+            // 
+            // InvButton
+            // 
+            this.InvButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.InvButton.Location = new System.Drawing.Point(0, 329);
+            this.InvButton.Name = "InvButton";
+            this.InvButton.Size = new System.Drawing.Size(151, 41);
+            this.InvButton.TabIndex = 16;
+            this.InvButton.Text = "View Inventory";
+            this.InvButton.UseVisualStyleBackColor = true;
+            this.InvButton.Click += new System.EventHandler(this.InvButton_Click);
+            // 
+            // CheckBtn
+            // 
+            this.CheckBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.CheckBtn.Location = new System.Drawing.Point(0, 288);
+            this.CheckBtn.Name = "CheckBtn";
+            this.CheckBtn.Size = new System.Drawing.Size(151, 41);
+            this.CheckBtn.TabIndex = 15;
+            this.CheckBtn.Text = "Check In / Out";
+            this.CheckBtn.UseVisualStyleBackColor = true;
+            this.CheckBtn.Click += new System.EventHandler(this.CheckBtn_Click);
+            // 
+            // ServBtn
+            // 
+            this.ServBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ServBtn.Location = new System.Drawing.Point(0, 247);
+            this.ServBtn.Name = "ServBtn";
+            this.ServBtn.Size = new System.Drawing.Size(151, 41);
+            this.ServBtn.TabIndex = 14;
+            this.ServBtn.Text = "Service Appointment";
+            this.ServBtn.UseVisualStyleBackColor = true;
+            this.ServBtn.Click += new System.EventHandler(this.ServBtn_Click);
+            // 
+            // DelCust
+            // 
+            this.DelCust.Dock = System.Windows.Forms.DockStyle.Top;
+            this.DelCust.Location = new System.Drawing.Point(0, 218);
+            this.DelCust.Name = "DelCust";
+            this.DelCust.Size = new System.Drawing.Size(151, 29);
+            this.DelCust.TabIndex = 13;
+            this.DelCust.Text = "Delete";
+            this.DelCust.UseVisualStyleBackColor = true;
+            this.DelCust.Click += new System.EventHandler(this.DelCust_Click);
+            this.DelCust.MouseEnter += new System.EventHandler(this.DelCust_Hover);
+            this.DelCust.MouseHover += new System.EventHandler(this.DelBtnLeave);
+            // 
+            // AddCust
+            // 
+            this.AddCust.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AddCust.Location = new System.Drawing.Point(0, 189);
+            this.AddCust.Name = "AddCust";
+            this.AddCust.Size = new System.Drawing.Size(151, 29);
+            this.AddCust.TabIndex = 12;
+            this.AddCust.Text = "Add";
+            this.AddCust.UseVisualStyleBackColor = true;
+            this.AddCust.Click += new System.EventHandler(this.AddCust_Click);
+            this.AddCust.MouseEnter += new System.EventHandler(this.AddCust_Hover);
+            this.AddCust.MouseHover += new System.EventHandler(this.AddBtnLeave);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 41);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Customer Management";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
             // 
             // RecepPage
             // 
@@ -223,6 +245,7 @@
             this.panel2.ResumeLayout(false);
             this.ControlPanel.ResumeLayout(false);
             this.ControlPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -231,18 +254,20 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button AddCust;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button DelCust;
-        private System.Windows.Forms.Button ServBtn;
-        private System.Windows.Forms.Button ProfBtn;
-        private System.Windows.Forms.Button InvButton;
-        private System.Windows.Forms.Button CheckBtn;
         private System.Windows.Forms.Button ExitButton;
         private System.Windows.Forms.Panel ControlPanel;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button ProfBtn;
+        private System.Windows.Forms.Button InvButton;
+        private System.Windows.Forms.Button CheckBtn;
+        private System.Windows.Forms.Button ServBtn;
+        private System.Windows.Forms.Button DelCust;
+        private System.Windows.Forms.Button AddCust;
+        private System.Windows.Forms.Button button1;
     }
 }
 
